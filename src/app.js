8b030f16f610b90537fe0a2e9a91e46306240141
ivocettour <ivocettour2@gmail.com>
2026-06-17 21:02:24 -0300

    tes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const openApiSpec = require('../docs/openapi.json');
 
-const app = express();
+cont app = express();
 
 app.get('/', (req, res) => {
   res.json({
